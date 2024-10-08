4954a7ef30137849f32ba9d83ae8d3998969855e
Bruno Valenzisi <bruno.valenzisi@hotmail.com>
2024-10-08 12:59:01 -0300

    terminando piramidal

--- src/problemas/empresa piramidal/auxiliares.hpp
+++ src/problemas/empresa piramidal/auxiliares.hpp
@@ -14,52 +14,70 @@
 #include "../../biblioteca/tads/parte2/Stack.hpp"
 using namespace std;
 
-struct Mov 
-{ 
-   int idCli; 
-   char mov; // 'E' o 'S' 
-   char hora[4]; // hhmm 
+struct Fecha{
+int dia;
+int mes;
+int anio;
 };
 
+Fecha fecha(int dia, int mes, int anio){
+Fecha f;
+f.dia=dia;
+f.mes=mes;
+f.anio=anio;
+return f;
+}
+string fechaMostrar(Fecha f){
+   string fecha;
+   fecha+=intToString(f.dia);
+   fecha+="/";
+   fecha+=intToString(f.mes);
+   fecha+="/";
+   fecha+=intToString(f.anio);
+   fecha+=" ";
+   return fecha;
+}
 
-
-struct Caja{
-int idCaja;
-Queue<int> cola;
-int longMax=0;
-int esperaTotal=0;
-int ocioTot=0;
-char ultimaSalida[4];
-char ultimaEntrada[4];
+struct Socio
+{
+   int idSocio;
+   char nombre[50];
+   int  idSocioRef;  // socio referente
+   Fecha fechaIngreso;
+   double totalVentasAcumuladas;
+   double liquidacionAnterior;
+   double liquidacionesAcumuladas;
 };
 
-
-struct Supermercado{
-int  cantCajas;
-Map<int,Caja> cajas;
-Map<int,int> clientes;
-int ocioTotal=0;
-int esperaTotal=0;
+struct RSocio{
+Socio s;
+double liquidacionActual;
 };
 
-Caja caja(int id,char apertura[4]){
-Caja c;
-c.idCaja=id;
-strcpy(c.ultimaSalida,apertura);
-strcpy(c.ultimaEntrada,"");
-return c;
+struct Venta
+{
+   int idSocio;
+   int idProducto;
+   char observ[100];
+   Fecha fecha; 
+   double importe;
+};
+
+RSocio rSocio(Socio s){
+RSocio rC;
+rC.s=s;
+double liquidacionActual=0;
+return rC;
 }
 
 
-Supermercado supermercado(int n){
-Supermercado s;
-s.cantCajas=n;
-for(int i=0;i<n;i++){
-Caja c= caja(i,"1000");
-mapPut<int,Caja>(s.cajas,n,c);
+Venta venta(int id,Fecha f,double monto ){
+Venta v;
+v.idSocio=id;
+v.importe=monto;
+v.fecha=f;
 }
-return s;
-}
+
 
 
 #endif
--- src/problemas/empresa piramidal/empresaPiramidal.cpp
+++ src/problemas/empresa piramidal/empresaPiramidal.cpp
@@ -19,27 +19,54 @@ int cmpIdSocio(RSocio rS,int b){
 return rS.s.idSocio-b;
 }
 
+int cmpRSocio(RSocio a, RSocio b){
+return a.s.idSocio,b.s.idSocio;
+}
+List<RSocio> cargarSocios(){
+List<RSocio> lst= list<RSocio>();
+FILE*f =fopen("SOCIOS.dat","r+b");
+Socio s=read<Socio>(f);
+while(!feof(f)){
+    RSocio rC=rSocio(s);
+    listOrderedInsert<RSocio>(lst,rC,cmpRSocio);
+    s=read<Socio>(f);
+}
+return lst; 
+}
+
 
 void procezarVenta(Venta v,List<RSocio> l){
 RSocio rC=*listFind<RSocio,int>(l,v.idSocio,cmpIdSocio);
 if(rC.s.idSocioRef!=-1){
     Venta vA=venta(rC.s.idSocioRef,v.fecha,v.importe*0.3*0.3);
     rC.liquidacionActual+=v.importe*0.3-v.importe*0.3*0.3;
+    rC.s.liquidacionesAcumuladas+=v.importe*0.3-v.importe*0.3*0.3;
+    rC.s.totalVentasAcumuladas+=v.importe;
     procezarVenta(vA,l);
 }else{
 rC.liquidacionActual+=v.importe*0.3;
+rC.s.liquidacionesAcumuladas+=v.importe*0.3;
+rC.s.totalVentasAcumuladas+=v.importe;
 }
 }
 
 
 void mostrarSocios(List<RSocio> l){
-
-
-
+listReset<RSocio>(l);
+RSocio rC=*listNext<RSocio>(l);
+while(listHasNext(l)){
+cout<<"Socio Numero: "<<rC.s.idSocio<<endl;
+cout<<"Nombre: "<<rC.s.nombre<<endl;
+cout<<"Fecha de ingreso: "<<fechaMostrar(rC.s.fechaIngreso)<<endl;
+cout<<"Liquidacion anterior: "<<rC.s.liquidacionAnterior<<endl;
+cout<<"Liquidacion actual: "<<rC.liquidacionActual<<endl;
+cout << "porcentaje de " << ((rC.liquidacionActual > rC.s.liquidacionAnterior) ? "aumento" : "decremento") << abs((rC.liquidacionActual-rC.s.liquidacionAnterior)/rC.s.liquidacionAnterior*100) << endl;
+cout<<"Total Acumulado: "<<rC.s.liquidacionesAcumuladas<<endl;
+}
 }
 
 int main(){
-List<RSocio> rSocios=list<RSocio>();
+List<RSocio> rSocios=cargarSocios();
 FILE* f=fopen("VENTAS.dat","r+b");
 Venta v=read<Venta>(f);
 while(!feof(f)){
